0f658e0a054d2c331d0aa16f16a7a42b176ab63e
dmurambadoro <dkmurambadoro@gmail.com>
2026-07-06 08:19:52 -0500

    add functionality to function stubs

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,28 +9,45 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    const total = 0;
+    cart.forEach(item => {
+      total = parseFloat(item.cost.substring(1)) * item.quantity;
+    });
+    return total;
   };
 
   const handleContinueShopping = (e) => {
-   
+    onContinueShopping(e);
   };
 
-
+  const handleCheckoutShopping = (e) => {
+    alert('Functionality to be added for future reference');
+  };
 
   const handleIncrement = (item) => {
+    dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1 }));
   };
 
   const handleDecrement = (item) => {
-   
+    if (item.quantity > 0) {
+      dispatch(updateQuantity({ name: item.name, quantity: item.quantity - 1 }));
+    } else { 
+      dispatch(removeItem(item));
+     }
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem(item.name));
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    return item.quantity * parseFloat(item.cost.substring(1));
   };
+
+  // more precise without a return statement
+  //const calculateTotalCost_alt = (item) =>
+  //  item.quantity * parseFloat(item.cost.replace("$", ""));
 
   return (
     <div className="cart-container">
